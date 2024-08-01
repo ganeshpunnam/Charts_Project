e097196e47f143e80e65bdcbae468f60d9a40124
ganeshpunnam <punnamganesh752@gmail.com>
2024-08-01 17:55:45 +0530

    Updated

--- src/Header.js
+++ src/Header.js
@@ -5,7 +5,11 @@ const Header = () => {
     <div className="header">
       <h1>Olympics Medals</h1>
       <p>Name: Punnam Sai Ganesh</p>
-      <p>Email: punnamganesh752@gmail.com</p>
+      <div className='HeaderEmail'>
+      <p>Email:</p>
+      <a id='HeaderEmailLink' href="mailto:punnamganesh752@gmail.com" className="email-link"> Punnamganesh752@gmail.com</a>
+
+      </div>
       <p>Contact: 9542455696</p>
     </div>
   );

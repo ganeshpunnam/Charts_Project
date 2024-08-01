7785e4de0211611c057ef520735e6f236f7a8ade
ganeshpunnam <punnamganesh752@gmail.com>
2024-08-01 19:01:38 +0530

    Upadted

--- src/Components/Chart.js
+++ src/Components/Chart.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import '../App.css';
+
 
 const Chart = ({ data }) => {
   return (
     <ResponsiveContainer width="100%" height={400}>
-      <LineChart data={data} margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
+      <LineChart data={data} className='Chartbox'>
         <CartesianGrid strokeDasharray="3 3" />
         <XAxis dataKey="Year" />
         <YAxis />
